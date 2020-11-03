cadda9e630ec7cc0396bc655f0620c945a7df024
ramankarki <ramankarki40@gmail.com>
2020-11-03 08:06:28 +0545

    resetting everything and starting from scratch

--- js/index.js
+++
@@ -1,7 +0,0 @@
-let hamburgur_btn = document.querySelector(".hamburgur-btn").addEventListener("click", run);
-let aside = document.querySelector(".aside-right");
-
-function run() {
-    aside.classList.toggle("aside-transition");
-
-}
